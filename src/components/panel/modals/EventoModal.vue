8277739610e1885ed7f16b73d37665ff002d71a0
Jamel <strangerdante@gmail.com>
2025-02-20 09:48:36 -0500

    mejorado modo oscuro modal

--- src/components/panel/modals/EventoModal.vue
+++ src/components/panel/modals/EventoModal.vue
@@ -247,12 +247,13 @@ const handleSubmit = async () => {
                 <select
                   v-model="selectedImageOption"
                   id="imagen"
-                  class="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer"
+                  class="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-teal-500 focus:outline-none focus:ring-0 focus:border-teal-500 peer dark:bg-gray-700"
                 >
                   <option
                     v-for="option in imageOptions"
                     :key="option.value"
                     :value="option.value"
+                    class="dark:bg-gray-700 dark:text-gray-300"
                   >
                     {{ option.label }}
                   </option>
